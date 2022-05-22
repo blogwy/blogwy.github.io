00da3d4942d1f421fc2359397526ebcce6e64ead
wangyu <wangyu@wangyu.link>
2022-05-22 12:14:36 +0800

    Site updated: 2022-05-22 12:14:34

---
+++ interview/html-css-interview/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+let str = ''
+
+for (let index = 1; index < 15; index++) {
+  let data = fs.readFileSync(`${index}.md`, { encoding: 'utf-8' })
+  if (data !== '#') {
+    console.log(`${index}.md:`)
+    const arr = data.split('\n\n')
+    const title = arr[0].replace('# ', '')
+    console.log(title);
+    str += `* [${title}](html-css-interview/${index}.md)
+    `
+  }
+}
+
+console.log('----------str----------');
+
+console.log(str);
